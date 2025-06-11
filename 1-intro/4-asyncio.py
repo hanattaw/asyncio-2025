cb9cfc8ae2dc440c0a240ef0c4aec225f8059ea1
Kritchai <loma.kcps@gmail.com>
2025-06-11 11:44:27 +0700

    99 people and tell time in 4

--- 1-intro/4-asyncio.py
+++ 1-intro/4-asyncio.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import time
 
 async def make_burger(student_id):
+    start = time.time()
     print(f"[{datetime.now().strftime('%H:%M:%S')}] เริ่มทำเบอร์เกอร์ให้นักเรียนคนที่ {student_id}")
     
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...")
@@ -17,13 +18,15 @@ async def make_burger(student_id):
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 4. ห่อเบอร์เกอร์...")
     await asyncio.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id}!")
+    end = time.time()  # บันทึกเวลาสิ้นสุด
+
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id}! ใช้เวลา {end - start:.2f} วินาที\n")
 
 async def main():
     start = time.time()
 
     # สร้าง task async สำหรับนักเรียนแต่ละคน
-    tasks = [asyncio.create_task(make_burger(i)) for i in range(1, 4)]
+    tasks = [asyncio.create_task(make_burger(i)) for i in range(1, 100)]
 
     # รอให้ทุก task เสร็จ
     await asyncio.gather(*tasks)
