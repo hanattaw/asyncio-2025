b1700a5ecf586b05de3c0c5e313cea75b98e356d
Kritchai <loma.kcps@gmail.com>
2025-06-11 10:25:16 +0700

    Tell time

--- 1-intro/1-synchronous.py
+++ 1-intro/1-synchronous.py
@@ -2,21 +2,24 @@ import time
 from datetime import datetime
 
 def make_burger(student_id):
+    start = time.time()
     print(f"[{datetime.now().strftime('%H:%M:%S')}] เริ่มทำเบอร์เกอร์ให้นักเรียนคนที่ {student_id}")
     
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...\n")
     time.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] 2. ทอดไก่...")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] 2. ทอดไก่...\n")
     time.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] 3. ใส่ผักและชีส...")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] 3. ใส่ผักและชีส...\n")
     time.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] 4. ห่อเบอร์เกอร์...")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] 4. ห่อเบอร์เกอร์...\n")
     time.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id}!")
+    end = time.time()  # บันทึกเวลาสิ้นสุด
+
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id} ใช้เวลา {end - start:.2f} วินาที\n")
 
 def main():
     start = time.time()
